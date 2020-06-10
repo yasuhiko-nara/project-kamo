e5f4f2050ccbaa1412b46796d01b9bdeddd69610
Yasuhiko Nara <narayasuhiko3@gmail.com>
2020-06-10 12:39:07 +0900

    small modification

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,6 +1,13 @@
 module.exports = {
   client: "pg",
-  connection: require("./config").connection,
+  connection: process.env.DB_URL || {
+    host: process.env.DB_HOST || "127.0.0.1",
+    port: process.env.DB_PORT || 5432,
+    database: process.env.DB_NAME || "firstpjt",
+    user: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASSWORD || "postgres",
+    charset: "utf8",
+  },
   pool: {
     min: 2,
     max: 10,
